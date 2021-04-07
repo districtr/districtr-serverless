e50a84f16ece082fb0644cea73fea18c0cbaad3c
JN <jenniniels.matthews@gmail.com>
2021-04-07 14:51:28 -0400

    parse state name and generalize

--- blockAssignPython/lambda_function.py
+++ blockAssignPython/lambda_function.py
@@ -4,19 +4,21 @@ import boto3
 s3 = boto3.client('s3')
 
 
-def block_assignment_from_blockgroups(assignment, blockgroup_mapping):
+def block_assignment_from_units(assignment, unit_to_block_mapping):
     """ Create block assignment file from given districtr assignment
 
     Args:
-        assignment (dict<str, int>): mapping between districtr units and district ids
-        blockgroup_mapping (dict<str, str>): mapping between blockgroups geoids and block geoids
+        assignment (dict<str, int>): mapping between districtr units (blockgroups/precincts) and 
+                                     district ids
+        unit_to_block_mapping (dict<str, str>): mapping between districtr units ids (GEOIDS, etc.)
+                                                and block geoids
 
     Returns:
         dict<str, int>: mapping between block geoids and district ids
     """
     block_assignment = {}
-    for bg, dist in assignment.items():
-        for block in blockgroup_mapping[bg]:
+    for unit, dist in assignment.items():
+        for block in unit_to_block_mapping[unit]:
             block_assignment[block] = dist
     return block_assignment
 
@@ -24,7 +26,7 @@ def block_assignment_from_blockgroups(assignment, blockgroup_mapping):
 def lambda_handler(event, context):
     # TODO implement
     bucket = "districtr"
-    state = event["state"]
+    state = event["state"].lower().replace(" ", "_")
     units = event["units"]
     assignment = event["assignment"]
     key = "block_assign/{}_{}.json".format(state, units)
@@ -32,7 +34,7 @@ def lambda_handler(event, context):
     try:
         data = s3.get_object(Bucket=bucket, Key=key)
         state_mapping = json.load(data['Body'])
-        block_assignment = block_assignment_from_blockgroups(assignment, state_mapping)
+        block_assignment = block_assignment_from_units(assignment, state_mapping)
         return {
             'statusCode': 200,
             'body': json.dumps(block_assignment)
