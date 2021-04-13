c1968889faad017288897cfccf05cf50e13e4503
JN <jenniniels.matthews@gmail.com>
2021-04-13 15:37:59 -0400

    Working version of VRA effectiveness lambda; add Json Parser; add texas resources; add additional dependencies

--- VRAEffectiveness/src/VRAEffectiveness/Function.fs
+++ VRAEffectiveness/src/VRAEffectiveness/Function.fs
@@ -1,22 +1,54 @@
-namespace LambdaFunc
-
-
+namespace VRAEffectivenessLambda
 open Amazon.Lambda.Core
+open Amazon.Lambda.APIGatewayEvents
+// open Amazon.Lambda.Serialization
+open FSharp.Data
+open Deedle
+open VRAEffectiveness
+open Newtonsoft.Json
 
-open System
+
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [<assembly: LambdaSerializer(typeof<Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer>)>]
 ()
 
+
+
 type Function() =
+    let (+/) path path' = System.IO.Path.Combine(path, path')
+    let PathCombine path path' path'' = System.IO.Path.Combine(path, path', path'')
+
+    let StateSuccessFunction: Map<string, SuccessFunction<string>> = Map.ofArray [|"texas", CoCCarriesElectTX; "louisiana", CoCCarriesElectLA|]
+
+    static member executingAssembly = System.Reflection.Assembly.GetExecutingAssembly().Location
+    static member executingAssemblyDir = System.IO.Path.GetDirectoryName Function.executingAssembly
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
     /// <param name="input"></param>
-    /// <param name="context"></param>
     /// <returns></returns>
-    member __.FunctionHandler (input: string) (_: ILambdaContext) =
-        match input with
-        | null -> String.Empty
-        | _ -> input.ToUpper()
+    member __.FunctionHandler (input: APIGatewayProxyRequest) (_: ILambdaContext) =
+        // unpack request
+        let request = JsonValue.Parse(input.Body)
+        let stateName = request.["state"].AsString().ToLower().Replace(" ", "_")
+        let PrecinctID = request.["precID"].AsString()
+        let districtID = "District"
+        let assignment = request.["assignment"].Properties() |> Array.map (fun (a,b) -> a,b.AsInteger())
+
+        let JsonFile = sprintf "%s.json" <| stateName |> PathCombine Function.executingAssemblyDir "resources"
+        let CsvFile = sprintf "%s.csv" <| stateName |> PathCombine Function.executingAssemblyDir "resources"
+        
+        let JsonValue = JsonValue.Load(JsonFile)
+        let StateData = Frame.ReadCsv(CsvFile) |> Frame.indexRowsString PrecinctID
+        let PlanData = StateData |> Frame.addCol districtID (assignment |> Series.ofObservations)
+
+        let VRAparser = Parser JsonValue
+        let Minorities = VRAparser.Minorities
+        let Elections = VRAparser.Elections
+        let CoCSuccess = StateSuccessFunction.[stateName]
+        
+        let vrascores: PlanVRASummary<int> = PlanVRAEffectivenessDetailed PlanData districtID Minorities Elections CoCSuccess
+        
+        JsonConvert.SerializeObject vrascores
--- VRAEffectiveness/test/VRAEffectiveness.Tests/FunctionTest.fs
+++ VRAEffectiveness/test/VRAEffectiveness.Tests/FunctionTest.fs
@@ -3,8 +3,9 @@ namespace VRAEffectiveness.Tests
 
 open Xunit
 open Amazon.Lambda.TestUtilities
+open Amazon.Lambda.APIGatewayEvents
 
-open LambdaFunc
+open VRAEffectivenessLambda
 
 
 module FunctionTest =
@@ -12,10 +13,20 @@ module FunctionTest =
     let ``Invoke ToUpper Lambda Function``() =
         // Invoke the lambda function and confirm the string was upper cased.
         let lambdaFunction = Function()
+        // let request = APIGatewayProxyRequest()
         let context = TestLambdaContext()
-        let upperCase = lambdaFunction.FunctionHandler "hello world" context
+        let testInput ="{\"state\": \"Texas\",\"precID\": \"CNTYVTD\",\"assignment\": {\"30001\": 1,\"30002\": 1,\"30003\": 1, \"30004\": 1,\"90001\": 1,\"90002\": 1,\"90003\": 1,\"90004\": 1,\"90005\": 1,\"90006\": 1,\"90007\": 1,\"90008\": 1,\"90009\": 1, \"90010\": 1, \"90011\": 1, \"110101\": 0,\"110201\": 0, \"110202\": 0, \"110301\": 0, \"110303\": 0, \"110401\": 0}}"        // let request = new APIGatewayProxyRequest({Body= testInput})
+        let upperCase = lambdaFunction.FunctionHandler testInput context
+        let expectation = "/Users/smaug/Documents/MGGG/districtr-serverless/VRAEffectiveness/src/VRAEffectiveness/texas.json"
+        Assert.Equal(expectation, upperCase)
 
-        Assert.Equal("HELLO WORLD", upperCase)
+
+    [<Fact>]
+    let ``Trial``() =
+        // printfn "%s" <| Function.executingAssembly
+        // printfn "%s" <| Function.executingAssemblyDir
+        
+        Assert.True <| true
 
     [<EntryPoint>]
     let main _ = 0
