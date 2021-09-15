c88485e3b50fd30483f2927216773167a416da84
Nick Doiron <ndoiron@mapmeld.com>
2021-09-15 08:43:35 -0400

    unassigned unit limits

--- unassignedUnits/lambda_function.py
+++ unassignedUnits/lambda_function.py
@@ -1,17 +1,16 @@
 import json
 import networkx as nx
+import pandas as pd
 from networkx.readwrite import json_graph
 import boto3
 
 s3 = boto3.client('s3')
 
-def connected_componets(graph):
+def connected_components(graph):
     return [list(comp) for comp in nx.connected_components(graph)]
     
 
 def lambda_handler(event, context):
-    # TODO implement
-    
     if 'body' in event.keys():
         event = json.loads(event["body"])
     bucket = "districtr"
@@ -20,18 +19,33 @@ def lambda_handler(event, context):
     plan_assignment = event["assignment"]
     assigned_units = set(plan_assignment.keys())
     key = "dual_graphs/{}_{}.json".format(state, units)
+    centroid_key = "centroids/{}_{}.csv".format(state, units)
 
     try:
         data = s3.get_object(Bucket=bucket, Key=key)
         graph = json_graph.adjacency_graph(json.load(data['Body']))
         graph.remove_nodes_from(assigned_units)
+        
+        hole_ids = connected_components(graph)
+        largest_segment_ids = (sorted(hole_ids, key=lambda x: len(x), reverse=True))[0]
+        
+        ctr_data = s3.get_object(Bucket=bucket, Key=centroid_key)
+        unit_centroids = pd.read_csv(ctr_data['Body'], dtype={"GEOID": str}).set_index("GEOID")
+        coords = unit_centroids.loc[largest_segment_ids]
+        
+
         return {
             'statusCode': 200,
             'body': json.dumps({
-                        "unnassigned_units": connected_componets(graph)}),
+                        "unassigned_units": [
+                            float(coords.LNG.min()),
+                            float(coords.LAT.min()),
+                            float(coords.LNG.max()),
+                            float(coords.LAT.max()),
+                            
+                        ] }),
         }
     
     except Exception as e:
         print(e)
         raise e
-    
