dc1dbefaeae8d4f4e50543b05108fd3948b3b328
Nick Doiron <ndoiron@mapmeld.com>
2021-10-03 23:54:38 -0400

    Create lambda_function.py

---
+++ s3Capabilities/lambda_function.py
@@ -0,0 +1,42 @@
+# return JSON description on whether S3 has files to calculate number markers, contiguity, and shapefile export
+import boto3
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event, context):
+    if 'body' in event.keys():
+        event = json.loads(event["body"])
+    state = event["state"].lower().replace(" ", "_")
+    units = event["units"].lower().replace(" ", "")
+    
+    bucket = "districtr"
+    
+    try:
+        key = "centroids/{}_{}.csv".format(state, units)
+        s3.head_object(Bucket=bucket, Key=key)
+        has_centroid = True
+    except Exception as e:
+        has_centroid = False
+
+    try:
+        key = "dual_graphs/{}_{}.csv".format(state, units)
+        s3.head_object(Bucket=bucket, Key=key)
+        has_graph = True
+    except Exception as e:
+        has_graph = False
+
+    try:
+        key = "shapefiles/{}_{}.csv".format(state, units)
+        s3.head_object(Bucket=bucket, Key=key)
+        has_shp = True
+    except Exception as e:
+        has_shp = False
+
+    return {
+            'statusCode': 200,
+            'body': json.dumps({
+                    'has_centroid': has_centroid,
+                    'has_graph': has_graph,
+                    'has_shp': has_shp,
+                })
+        }
